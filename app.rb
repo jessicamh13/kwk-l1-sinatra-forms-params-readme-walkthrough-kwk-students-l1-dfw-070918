10acf21023eaf76f875242167f97ec39b0bd8aa6
jessicamh13 <jessica.mh13@yahoo.com>
2018-07-17 20:51:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,11 @@ class App < Sinatra::Base
     erb :food_form
   end
 
-  
+  <form>
+  <p>Your Name: <input type="text"></p>
+  <p>Your Favorite Food: <input type="text"></p>
+  <input type="submit">
+</form>
   
   # Add your post route and action below
 
